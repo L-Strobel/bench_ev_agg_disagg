8595376235495372d6decc3328a6c5121ad51a4d
Leo Strobel <leo.strobel@uni-wuerzburg.de>
2024-03-23 11:12:10 +0100

    American date format

--- python_code/cost_ts.py
+++ python_code/cost_ts.py
@@ -34,7 +34,7 @@ def real_costs(ndays: int, seed: int, fn: str) -> np.ndarray:
     # Load data
     spot_prices = pd.read_csv(
         fn, decimal=".", sep=";",
-        na_values="-", thousands=",", parse_dates=["Date"], dayfirst=True
+        na_values="-", thousands=",", parse_dates=["Date"]
     )
     spot_prices["weekday"] = spot_prices.Date.dt.isocalendar()["day"]
 
@@ -44,7 +44,7 @@ def real_costs(ndays: int, seed: int, fn: str) -> np.ndarray:
     ].Date.unique())
     start_date = rng.choice(starte_dates)
     start_idx = spot_prices[
-        (spot_prices.Date == start_date) & (spot_prices.Anfang == "00:00")
+        (spot_prices.Date == start_date) & (spot_prices.Start == "12:00 AM")
     ].index[0]
 
     # Normalize
@@ -67,11 +67,11 @@ def future_costs(ndays: int, seed: int, fn_gen: str, fn_dem: str) -> np.ndarray:
     # Load data
     generation = pd.read_csv(
         fn_gen, decimal=".", sep=";",
-        na_values="-", thousands=",", parse_dates=["Date"], dayfirst=True
+        na_values="-", thousands=",", parse_dates=["Date"]
     )
     load = pd.read_csv(
         fn_dem, decimal=".", sep=";",
-        na_values="-", thousands=",", parse_dates=["Date"], dayfirst=True
+        na_values="-", thousands=",", parse_dates=["Date"]
     )
     load["weekday"] = load.Date.dt.isocalendar()["day"]
 
@@ -88,7 +88,7 @@ def future_costs(ndays: int, seed: int, fn_gen: str, fn_dem: str) -> np.ndarray:
         (load.weekday == 1) & (load.index + 96*ndays <= 96*365)
     ].Date.unique())
     start_date = rng.choice(starte_dates)
-    start_idx = load[(load.Date == start_date) & (load.Anfang == "00:00")].index[0]
+    start_idx = load[(load.Date == start_date) & (load.Start == "12:00 AM")].index[0]
 
     # Normalize
     costs = load.loc[start_idx: start_idx+ndays*96-1, 'Residual_load_synthetic_2045'].values
